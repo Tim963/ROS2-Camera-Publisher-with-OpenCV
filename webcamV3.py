c21280742700cf87c4bf0d19ff3a2f5027796963
Tim963 <tim-lumpp@online.de>
2025-05-18 23:32:58 +0200

    Update webcamV3.py

--- webcamV3.py
+++ webcamV3.py
@@ -9,21 +9,21 @@ class CameraNode(Node):
         super().__init__('camera_publisher')
         # Publisher für Farbbild
         self.publisher_color = self.create_publisher(Image, 'image_raw', 10)
-        # NEU: Publisher für Graustufenbild
+        # Publisher für Graustufenbild
         self.publisher_gray = self.create_publisher(Image, 'image_gray', 10)
         
         self.timer = self.create_timer(0.1, self.timer_callback)
-        self.cap = cv2.VideoCapture(2)  # Kamera-Index anpassen
+        self.cap = cv2.VideoCapture(2)  # Kamera-Index anpassen find out with: ls /dev/video*
         self.bridge = CvBridge()
 
     def timer_callback(self):
         ret, frame = self.cap.read()
         if ret:
-            # Farbbild publizieren (wie bisher)
+            # Farbbild publizieren 
             msg_color = self.bridge.cv2_to_imgmsg(frame, 'bgr8')
             self.publisher_color.publish(msg_color)
             
-            # NEU: Graustufenbild erstellen und publizieren
+            # Graustufenbild erstellen und publizieren
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             msg_gray = self.bridge.cv2_to_imgmsg(gray_frame, 'mono8')
             self.publisher_gray.publish(msg_gray)
